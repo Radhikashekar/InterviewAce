6b23720616ef1e9e947623966f1eb1dd2563e8db
Radhika Shekhar <radhikashekar2005@gmail.com>
2026-07-11 23:07:00 +0530

    Addes resusable function and continuous quiz menu

--- app.py
+++ app.py
@@ -1,22 +1,74 @@
 topics= ["python","Git","Linux","Exit"]
 
-print("========================")
-print("Welcome to InterviewAce")
-print("========================")
+questions = {
+              "python":"""What is Python?
+              
+              A. Database
+              B. programming Language
+              C. Operating system
+              D. Browser""",
 
-for i in range(len(topics)):
+            "Git":"""What is git?
+            
+            A. Database
+            B. Version Control System
+            C. Operating System
+            D. Web Broser""",
+
+            "Linux":"""What is linux?
+            
+            A. Programming language
+            B. Operating System
+            C. Database
+            D.Browser""",
+}
+
+
+
+def ask_question(topic, correct_answer):
+    global score
+
+    print(f"\n{topic} Interview")
+    print(questions[topic])
+
+    answer = input("Enter your answer(A/B/C/D):").upper()
+
+    score = 0
+
+    if answer == correct_answer :
+            score += 1
+            print("correct")
+            
+    else:
+            print("Wrong")
+
+    print("Your score:", score)
+
+running = True
+
+while running:
+
+  for i in range(len(topics)):
     print(f"{i + 1}. {topics[i]}")
 
-choice = int(input("Enter your choice:"))
+  choice = int(input("Enter your choice:"))
 
-if choice == 1 :
-    print("You selected Python.")
-elif choice  == 2:
-    print("You selected Git.")
-elif choice  == 3:
-    print("You selected Linux.")
-elif choice  == 4:
-    print("You selected Exit.")
 
-else:
-  print("Invalid Choice.")
+
+  if choice == 1:
+    ask_question("python","B")
+
+  elif choice == 2:
+    ask_question("Git","B")
+
+  elif choice == 3:
+    ask_question("Linux", "B")
+
+  elif choice == 4:
+     print("Thank you for using interviewAce")
+
+     running = False
+
+  else:
+    print("Inavlid choice")
+    
